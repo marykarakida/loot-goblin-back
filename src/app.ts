1fb7136e8d9bb0ef4be3cc5a810deeea3503bb43
Mary Karakida <makarakida@gmail.com>
2022-09-28 15:45:05 -0300

    create basic file structure

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,13 @@ import cors from 'cors';
 import express, { json } from 'express';
 import 'express-async-errors';
 
+import router from './routes/router';
+
 const app = express();
 
 app.use(cors());
 app.use(json());
+
+app.use(router);
 
 export default app;
