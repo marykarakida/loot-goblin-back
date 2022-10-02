1dda577509976f058431f64421d4f7e25a1e2eb9
Mary Karakida <makarakida@gmail.com>
2022-10-01 22:59:56 -0300

    fix: delete passwordConfirmation field to create an account

--- src/schemas/userSchema.ts
+++ src/schemas/userSchema.ts
@@ -2,11 +2,8 @@ import Joi from 'joi';
 
 export const createUserSchema = Joi.object({
     email: Joi.string().trim().email().required(),
-    username: Joi.string().trim().required(),
+    username: Joi.string().trim().min(3).max(24).required(),
     password: Joi.string().trim().min(8).max(24).required(),
-    passwordConfirmation: Joi.string().required().valid(Joi.ref('password')).messages({
-        'any.only': "'passwordConfirmation' has to match 'password'",
-    }),
     picture: Joi.string().trim().uri().required(),
 });
 
